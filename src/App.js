9270c8adc382fecd2b1eb7032d1a136c32a5ee0f
Faran Javed <howtocodefaran@outlook.com>
2022-05-08 14:32:12 +0500

    make layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import FormCard from "./components/FormCard";
+import Users from "./components/Users";
 
 function App() {
   return (
-    <div className="card-center">
+    <div className="card-center card-w-50">
       <FormCard></FormCard>
+      <Users></Users>
     </div>
   );
 }
---
+++ src/components/Users.js
@@ -0,0 +1,7 @@
+import "./Users.css";
+
+const Users = () => {
+  return <div className="card users-card">user list</div>;
+};
+
+export default Users;
